a7b2be04d711f0978132abc442ebc7326d5e17d3
Eniekpo <johneniekpo4life@gmail.com>
2024-01-18 23:53:50 +0100

    a

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.title(':bar_chart: STREAMLIT TUTORIALS')
+st.subheader('I am a sub-header')
+st.header('I am a header')
+st.text('This is a text')
+st.markdown('## *This is a Markdown*')
+st.markdown('**This is a *Markdown* bold text**')
+
+st.caption("This is a Caption")

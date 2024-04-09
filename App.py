60adced2ff3bf793a875576a6e057d477a4562d9
Eniekpo <johneniekpo4life@gmail.com>
2024-04-09 18:39:30 +0100

    a

--- App.py
+++ App.py
@@ -1,7 +1,6 @@
 import streamlit as st
-import plotly.express as px
 import pandas as pd
-import os
+import numpy as np
 import warnings
 warnings.filterwarnings('ignore')
 
@@ -13,11 +12,23 @@ st.set_page_config(
 st.title(" :bar_chart: YOLETECH HUB DATA ANALYTICS")
 st.markdown('<style>div.block-container{padding-top: 1rem;}</style>', unsafe_allow_html=True)
 
-fl = st.file_uploader(":file_folder: Upload a File", type=(["csv", "txt", "xlsx", "xls"]))
-if fl is not None:
-    filename = fl.name
-    st.write(filename)
-    df = pd.read_csv(filename)
-else:
-    os.chdir(r"C:\Users\YOLETECH\Dropbox\STREAMLIT")
-    df = pd.read_csv('Sales_report.csv')
+# Loading The Data 
+df = ('Sales_report.csv')
+
+@st.cache_data
+def load_data(nrows):
+    data = pd.read_csv(df, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    return data
+
+data_load_state = st.text('Loading data...')
+data = load_data(10000)
+data_load_state.text("Done! (using st.cache_data)")
+
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+
+
+st.subheader('Profit By Location')

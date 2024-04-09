c4779d69e4916281d90142e750b15da3487e23e3
Eniekpo <johneniekpo4life@gmail.com>
2024-04-09 13:49:17 +0100

    a

--- App.py
+++ App.py
@@ -17,22 +17,7 @@ fl = st.file_uploader(":file_folder: Upload a File", type=(["csv", "txt", "xlsx"
 if fl is not None:
     filename = fl.name
     st.write(filename)
-    df = pd.read_excel(filename)
+    df = pd.read_csv(filename)
 else:
     os.chdir(r"C:\Users\YOLETECH\Dropbox\STREAMLIT")
-    df = pd.read_excel('Sales_report.csv')
-
-col1, col2 = st.columns((2))
-df["Month"] = pd.to_datetime(df["Month"])
-
-# Getting the MIN and MAX Date
-startDate = pd.to_datetime(df["Month"]).min()
-endDate = pd.to_datetime(df["Month"]).max()
-
-with col1:
-    date1 = pd.to_datetime(st.date_input('Start Date', startDate))
-
-with col2:
-    date2 = pd.to_datetime(st.date_input('End Date', endDate))
-
-df = df[(df["Month"] >= date1) & (df["Month"] <= date2)].copy()
+    df = pd.read_csv('Sales_report.csv')

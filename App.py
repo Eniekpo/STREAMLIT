4fb5be4f438ead60dc4b1782084faa9a2b7adf4f
Eniekpo <johneniekpo4life@gmail.com>
2024-04-12 17:52:36 +0100

    a

--- App.py
+++ App.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import plotly.express as px
 
 import numpy as np
 import warnings
@@ -25,6 +26,7 @@ def load_data(nrows):
     data.rename(lowercase, axis='columns', inplace=True)
     return data
 
+# Show Data 
 data_load_state = st.text('Loading data...')
 data = load_data(10000)
 data_load_state.text("Done! (using st.cache_data)")
@@ -33,4 +35,45 @@ if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(data)
 
-st.subheader('DATA VISUALIZATIONS')
+# View and Download Data
+with st.expander("View Data"):
+    st.write(data.iloc[:200,1:20:2].style.background_gradient(cmap="Oranges"))
+
+# Download orginal DataSet
+csv = data.to_csv(index = False).encode('utf-8')
+st.download_button('Download Data', data = csv, file_name = "Data.csv",mime = "text/csv")
+
+st.subheader('SALES DASHBOARD')
+
+# Draw a Pie Chart
+st.header('Profit By Month')
+
+fig = px.pie(data, values = 'profit', names='month', hole=0.5)
+fig.update_traces(text=data['profit'], textposition='outside')
+st.plotly_chart(fig, use_container_width=True)
+
+# A bar Chart
+st.subheader("Profit By Location")
+fig = px.bar(data, x = 'sale_location', y = 'profit' ,template='seaborn')
+st.plotly_chart(fig, use_container_width=True, height = 200)
+
+# A Column Chart
+st.subheader('Profit By Education')
+fig = px.bar(data, x = 'education', y = 'profit' ,template='seaborn')
+st.plotly_chart(fig, use_container_width=True, height = 200)
+
+# Create a treemap based on Region, Category and Sub-Category
+st.subheader('Hierachical view of Profit using TreeMap')
+fig = px.treemap(data, path=['month', 'sale_location', 'education'], values='profit', hover_data=['profit'],
+                  color='education')
+fig.update_layout(width=800, height=650)
+st.plotly_chart(fig, use_container_width=True)
+
+
+# Create a scatter plot
+data1 = px.scatter(data, x = "sale_location", y = "profit", size = "commission")
+data1['layout'].update(title="Relationship between Sales and Profits using Scatter Plot.",
+                       titlefont = dict(size=20),xaxis = dict(title="Profit",titlefont=dict(size=19)),
+                       yaxis = dict(title = "Profit", titlefont = dict(size=19)))
+st.plotly_chart(data1,use_container_width=True)
+
